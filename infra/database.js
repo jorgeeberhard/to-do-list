7a5feae4c60a5227ae28f4174c593a548722aec0
Jorge Miguel Eberhard da Conceição <jorgem.eberhardc@gmail.com>
2025-01-22 16:27:28 -0300

    add `ssl` to the production database

--- infra/database.js
+++ infra/database.js
@@ -1,12 +1,17 @@
 import postgres from "postgres";
 import "dotenv/config";
 
+function getSSLValues() {
+  return process.env.NODE_ENV === "production" ? true : false;
+}
+
 const sql = postgres("", {
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
+  ssl: getSSLValues(),
 });
 
 export default sql;
