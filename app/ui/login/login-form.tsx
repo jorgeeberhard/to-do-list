e5d940d4ce63fedd4e8990839be3d8b65562f09f
Jorge Miguel Eberhard da Conceição <jorgem.eberhardc@gmail.com>
2025-01-30 13:28:53 -0300

    change style from `/login` form

--- app/ui/login/login-form.tsx
+++ app/ui/login/login-form.tsx
@@ -16,7 +16,7 @@ export default function LoginForm() {
       <form className="bg-yellow-100">
         <div className="flex flex-col px-3 py-2">
           <input
-            className="bg-yellow-100"
+            className="bg-yellow-100 focus:outline-0"
             type="email"
             placeholder="email@domain.com.br"
           />
@@ -24,7 +24,7 @@ export default function LoginForm() {
 
         <div className="flex flex-col px-3 py-2">
           <input
-            className="bg-yellow-100"
+            className="bg-yellow-100 focus:outline-0"
             placeholder="Password"
             type="password"
             id="password"
