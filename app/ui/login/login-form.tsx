8372e9408681387c70bde3114563a1bdf6e72397
Jorge Miguel Eberhard da Conceição <jorgem.eberhardc@gmail.com>
2025-01-31 12:10:43 -0300

    add `login` button

--- app/ui/login/login-form.tsx
+++ app/ui/login/login-form.tsx
@@ -30,6 +30,14 @@ export default function LoginForm() {
             id="password"
           />
         </div>
+        <div className="flex justify-center">
+          <button
+            className="px-2 py-1 m-3 border border-blue-300 bg-blue-300 solid rounded-md"
+            type="submit"
+          >
+            Login
+          </button>
+        </div>
       </form>
     </section>
   );
