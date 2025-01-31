41fbf6f1372ed4deb05206d19cabceabf7bd5a51
Jorge Miguel Eberhard da Conceição <jorgem.eberhardc@gmail.com>
2025-01-31 12:32:23 -0300

    add responsiveness to the page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,7 @@ import LoginForm from "@/app/ui/login/login-form";
 export default function Login() {
   return (
     <div className="flex justify-center p-10">
-      <div className="flex flex-col w-1/3 justify-center">
+      <div className="flex flex-col w-2/3 md:w-1/3 justify-center">
         <Header className="flex justify-center" title="Login" />
         <LoginForm />
       </div>
